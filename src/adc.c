f9aab536e4b2c50d6690f1090d0013989c21f395
TanmayBhilkar <86276947+TanmayBhilkar@users.noreply.github.com>
2021-07-23 23:42:12 +0530

    Update adc.c

--- src/adc.c
+++ src/adc.c
@@ -1,13 +1,4 @@
-/**
- * @file adc.c
- * @author vinayak katigar (vvkatigar1999@gmail.com)
- * @brief for Activit2, Adc potentiometer is used for as an sensor
- * @version 0.1
- * @date 2021-04-25
- *
- * @copyright Copyright (c) 2021
- *
- */
+
 #include<avr/io.h>
 #include "adc.h"
 
