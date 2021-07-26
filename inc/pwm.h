37d999435dce111a65d1dc9689d65c7fb9b9bb98
TanmayBhilkar <86276947+TanmayBhilkar@users.noreply.github.com>
2021-07-26 23:38:07 +0530

    Update pwm.h

--- inc/pwm.h
+++ inc/pwm.h
@@ -1,15 +1,4 @@
-/**
- * @file pwm.h
- * @author vinayak katigar (vvkatigar1999@gmail.com)
- * @brief Display the output of the temperature sensor
- * @version 0.1
- * @date 2021-04-29
- *
- * @copyright Copyright (c) 2021
- *
- */
-
-#ifndef __PWM_H__
+ #ifndef __PWM_H__
 #define __PWM_H__
 #include<avr/io.h>
 
